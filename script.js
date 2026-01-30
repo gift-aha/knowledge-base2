c08aeb7c8c11004a4c22882d7a688f09b48deadd
gift-aha <yiahayi@outlook.com>
2026-01-30 10:40:36 +0800

    script.js

--- script.js
+++ script.js
@@ -3632,4 +3632,12 @@ document.addEventListener('click', function(event) {
     if (event.target === timelineEditModal) {
         UIManager.closeTimelineEditModal();
     }
+function sanitizeInput(input) {
+  if (typeof input !== 'string') return '';
+  return input
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#x27;');
+}
 });
